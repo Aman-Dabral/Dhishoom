9ac89075a5ee068e9f7ea2bf03e7ddacda4d056a
Aman-Dabral <66616295+Aman-Dabral@users.noreply.github.com>
2021-01-30 09:32:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const {createServer} = require("http");
 const data = require('./data.json');
-createServer((req, res) => {
-if(req.url.indexOf("/login/") == 0) {const [email, pass] = req.url.replace("/login/", "").split('/');
-for(let i = 0; i < data.length; i++){
-if (data[i].email == email && data[i].password == pass) res.end('{"login": true}')
-}
-res.end('{"login": false}')
-}
+createServer(function(req, res){
+if(req.url.indexOf("/login/") == 0) {
+  const [email, pass] = req.url.replace("/login/", "").split('/');
+  for(let i = 0; i < data.length; i++){
+    if (data[i].email == email && data[i].password == pass) {res.end('{"login": true}'); return;}
+  }
+  res.end('{"login": false}');
+}else res.writeHead(404)
 }).listen(80);
