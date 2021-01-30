3881713db3bf8d610ae7af572667213ce9522563
Aman-Dabral <66616295+Aman-Dabral@users.noreply.github.com>
2021-01-30 12:06:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const data = require('./data.json');
 createServer(function(req, res){
 if(req.url.indexOf("/login/") == 0) {
   const [email, pass] = req.url.replace("/login/", "").split('/');
+  res.writeHead(200, {"content-type": "text/html"});
   for(let i = 0; i < data.length; i++){
     if (data[i].email == email && data[i].password == pass) {res.end('{"login": true}'); return;}
   }
   res.end('{"login": false}');
-}else res.writeHead(404)
+}else {res.writeHead(404, {"content-type": "text/html"}); res.end("HI")}
 }).listen(80);
