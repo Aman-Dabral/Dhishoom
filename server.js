c76a67e8922cc6a7576c934186915ec2f902b04e
Aman-Dabral <66616295+Aman-Dabral@users.noreply.github.com>
2021-01-30 09:26:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const data = require('./data.json');
 createServer((req, res) => {
 if(req.url.indexOf("/login/") == 0) {const [email, pass] = req.url.replace("/login/", "").split('/');
 for(let i = 0; i < data.length; i++){
-if (data[i].email == email && data[i].password == password) res.end('{"login": true}')
+if (data[i].email == email && data[i].password == pass) res.end('{"login": true}')
 }
 res.end('{"login": false}')
 }
-}).listen(80)
+}).listen(80);
